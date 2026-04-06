971c2bcb493855a6fe76df5f2088349aa77c24db
Dhruv Tomar <dhruv.0307t@gmail.com>
2026-04-06 17:35:21 +0530

    final backend commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000' }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
